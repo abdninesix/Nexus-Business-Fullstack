50a307f186eb79060c55156cde6613276b6163a7
abdninesix <mohabd1995@gmail.com>
2025-08-28 18:18:06 +0500

    Updated

--- client/src/context/SocketContext.tsx
+++ client/src/context/SocketContext.tsx
@@ -79,10 +79,10 @@ const SocketHandler: React.FC = () => {
                 toast.custom((t) => (
                     <div
                         className={`${t.visible ? 'animate-enter' : 'animate-leave'
-                            } max-w-md w-full shadow-lg cursor-pointer rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}
+                            } max-w-md w-full shadow-lg cursor-pointer rounded-lg pointer-events-auto flex`}
                         onClick={() => {
                             navigate(path);
-                            toast.dismiss(t.id); // Dismiss the toast on click
+                            toast.dismiss(t.id);
                         }}
                     >
                         <div className="flex-1 w-0 p-4 bg-gray-900 text-white">
@@ -100,13 +100,13 @@ const SocketHandler: React.FC = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="flex border-l border-gray-200">
+                        <div className="bg-white flex border-l border-gray-200">
                             <button
                                 onClick={(e) => {
                                     e.stopPropagation(); // Prevent navigation
                                     toast.dismiss(t.id);
                                 }}
-                                className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-primary-600 hover:text-primary-500 focus:outline-none focus:ring-2 focus:ring-primary-500"
+                                className="w-full border border-transparent p-4 flex items-center justify-center text-sm font-medium text-primary-600 hover:text-primary-500 focus:outline-none focus:ring-2 focus:ring-primary-500"
                             >
                                 Close
                             </button>
