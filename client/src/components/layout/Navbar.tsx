c0a74bd51c1c3dd6e260b214b02758fca3933fce
abdninesix <mohabd1995@gmail.com>
2025-09-04 12:58:45 +0500

    Updated

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -122,7 +122,7 @@ export const Navbar: React.FC = () => {
 
   return (
     <nav className="bg-white shadow-md">
-      <div className="mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto">
         <div className="flex justify-between h-16">
           {/* Logo and brand */}
           <div className="flex-shrink-0 flex items-center">
